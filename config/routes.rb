3df6c71a0e565986e6965ae5dba4f92b634805c6
Jeff McHale <rhithiktheawesome@gmail.com>
2022-06-06 16:14:44 -0400

    started pulling in information from the other blog

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admins
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "articles#index"
+  root "articles#about"
   resources :articles do
     resources :comments
   end
