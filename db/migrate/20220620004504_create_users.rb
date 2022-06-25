a69ce16e0e80906e4058bda2da1c4db31e7a8d59
Benjamin Broestl <benjamin.broestl@exprealty.net>
2022-06-25 15:36:34 -0400

    Generate devise views. Simplify migrations. Add seed file

--- db/migrate/20220620004504_create_users.rb
+++ db/migrate/20220620004504_create_users.rb
@@ -1,9 +1,16 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :password
-      t.string :password_confirmation
+      t.boolean :admin
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_20_192823) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_20_004504) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,7 +67,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_20_192823) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "author"
-    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
@@ -84,12 +83,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_20_192823) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password"
-    t.string "password_confirmation"
+    t.boolean "admin"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
